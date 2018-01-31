aca900cb032a1729495677a9cdaf8285f62ebd05
Jacob Cunningham <jacob.joc@gmail.com>
2018-01-31 13:05:08 -0800

    Add files via upload

---
+++ 1.5.3PLTW/1.5.3PLTW/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace _1._5._3PLTW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Location = new System.Drawing.Point(120, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(641, 642);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Rectangle";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 65);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Circle";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.trackBar1.Location = new System.Drawing.Point(23, 242);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 302);
+            this.trackBar1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 226);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Size";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 144);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(762, 637);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ 1.5.3PLTW/1.5.3PLTW/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1._5._3PLTW
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        public Form1()
+        {
+            InitializeComponent();
+            g = pictureBox1.CreateGraphics();
+            trackBar1.Minimum = 10;
+            trackBar1.Maximum = 100;
+            trackBar1.TickFrequency = 5;
+            trackBar1.LargeChange = 5;
+            trackBar1.SmallChange = 1;
+            pictureBox1.MouseClick += new MouseEventHandler(draw);
+            button1.MouseClick += new MouseEventHandler(rectangle);
+            button2.MouseClick += new MouseEventHandler(circle);
+            button4.MouseClick += new MouseEventHandler(clear);
+        }
+        float type;
+        public void rectangle(object sender, MouseEventArgs e) {
+            type = 1;
+        }
+        public void circle(object sender, MouseEventArgs e)
+        {
+            type = 2;
+        }
+        
+        public void draw(object sender, MouseEventArgs e) {
+            float value = trackBar1.Value;
+            switch (type) {
+                case 1:
+                    g.FillRectangle(Brushes.Black, e.X, e.Y, value, value);
+                    break;
+                case 2:
+                    g.FillEllipse(Brushes.Black, e.X, e.Y, value, value);
+                    break;
+                default:
+                    g.FillEllipse(Brushes.Black, e.X, e.Y, value, value);
+                    break;
+            }
+        }
+        public void clear(object sender, MouseEventArgs e) {
+            g.Clear(Color.White);
+        }
+    }
+}
