9a679c630a37eebf64878ba2403abd8a8e002573
Jacob Cunningham <jacob.joc@gmail.com>
2018-01-31 13:04:26 -0800

    Add files via upload

---
+++ pltw1.5.3/pltw1.5.3/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pltw1._5._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.MouseClick += new MouseEventHandler(searchBox);
+        }
+        
+        public void searchBox(object sender, MouseEventArgs e) {
+            //Uri search = new Uri(textBox1.Text);
+            //webBrowser1.Url = search;
+            webBrowser1.Navigate("http://www.google.com");
+            //HtmlDocument doc = webBrowser1.Document;
+            //HtmlElement elem = doc.GetElementById("q");
+            
+        }
+
+    }
+}
